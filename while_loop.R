56d1415d60a081a9923194280574fa0a7be925c5
aman pathak <amanpathak8802@gmail.com>
2026-02-14 21:22:33 +0200

    read_while_loop

---
+++ while_loop.R
@@ -0,0 +1,25 @@
+# while loop 
+
+v<-c("hello","r","this","is ","data","scientist","aman")
+x<-2
+while(x<6){
+  print(v)
+  x<-x+1
+}
+#################.  Basic while Loop (Condition Controlled)
+x <- 1
+while(x <= 5){
+  print(x)
+  x <- x + 1}
+
+
+#################.  Infinite while Loop
+x <- 1
+while(TRUE){
+  print(x)
+  x <- x + 1
+  if(x > 5){
+    break }}
+
+
+##################while Loop With next
