e3726eb7924b2051a68076a1901d06e4339ddbb6
aman pathak <amanpathak8802@gmail.com>
2026-02-14 16:12:29 +0200

    operator

---
+++ operators.R
@@ -0,0 +1,63 @@
+#operators in r  
+#Arithmetic operator  = +, - ,*,/,%% remainder ,%/%  questiont , ^ power 
+a <- 9.25
+b <- 2
+print(a-b)  #substraction
+print(a+b)  #addition
+print(a*b)  #Multiple
+print(a/b)  #Division
+print(a%%b) #remainder
+print(a%/%b) #quotient
+print(a^b)  #power of
+
+
+#Vectors 
+c1 <- c(3,4,5) # this c is used to make vectors 
+print(typeof(c1))
+class(c1)
+c2 <-c(23,45,67)
+
+print(c1-c2)  #substraction  arthematic operator with vector 
+print(c1+c2)   #addition
+print(c1*c2)   #Multiple
+print(c1/c2)  #Division
+print(c1%%c2) #remainder
+print(c1%/%c2) #quotient
+print(c1^c2) #power of
+
+## Relational Operator  == , <> , <= ,>=,!= in short true or false 
+a<-9.5
+b<-2
+print(a<b)  #substraction    sol - false 
+print(a>b)  #a greater than b  sol True 
+print(a<=b)  #a less than b 
+print(a>=b)  #a greater than or eqoual to b 
+print(a==b) #a and b are equal 
+print(a!=b) #a is not equal to b 
+
+
+#Vectors 
+c1 <- c(3,4,5) # this c is used to make vectors 
+print(typeof(c1))
+class(c1)
+c2 <-c(23,45,67)
+
+print(c1<c2)  #substraction    sol - false 
+print(c1>c2)  #c1 greater than c2  sol True 
+print(c1<=c2)  #c1 less than c2 
+print(c1>=c2)  #c1 greater than or eqoual to c2 
+print(c1==c2) #c1 and c2 are equal 
+print(c1!=c2) #c1 is not equal to c2 
+
+#logical()  operator ->.   & , ! ,| , && ,||
+
+
+
+
+
+#assigment  operator
+
+c1 <- c(3,4,5)
+print(typeof(c1))
+class(c1)
+c2 <-c(23,45,67)
